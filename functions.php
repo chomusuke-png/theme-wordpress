d8a98346b2569671f1b7a505c7ed45f53440cad4
chomusuke-png <owokenji@gmail.com>
2025-12-01 09:35:23 -0300

    a

--- functions.php
+++ functions.php
@@ -71,18 +71,18 @@ function theme_enqueue_assets()
         filemtime(get_template_directory() . '/assets/css/button-top.css')
     );
     wp_enqueue_style(
-        'hero-slider',
-        get_template_directory_uri() . '/assets/css/hero-slider.css',
-        array(),
-        filemtime(get_template_directory() . '/assets/css/hero-slider.css')
-    );
-    wp_enqueue_style(
         'swiper-css',
         'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
         [],
         '11.0.0'
     );
-
+    wp_enqueue_style(
+        'hero-slider',
+        get_template_directory_uri() . '/assets/css/hero-slider.css',
+        array(),
+        filemtime(get_template_directory() . '/assets/css/hero-slider.css')
+    );
+    
     // Scripts
     wp_enqueue_script(
         'swiper-js',
